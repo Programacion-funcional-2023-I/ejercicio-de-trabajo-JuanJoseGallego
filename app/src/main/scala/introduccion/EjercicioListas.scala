e5d3d302c7c3467c50d96622e564d662d8c55917
Juan Jose <juan.gallego.calderon@correounivalle.edu.co>
2023-09-02 12:04:46 -0500

    tarea 1 realizada

--- app/src/main/scala/introduccion/EjercicioListas.scala
+++ app/src/main/scala/introduccion/EjercicioListas.scala
@@ -13,8 +13,20 @@ class EjercicioListas {
   */
   def repetirListas(lista: List[Int], n: Int): List[List[Int]] = {
     var listaRepetida : List[List[Int]] = List()
-    //Complete el código
-    throw new UnsupportedOperationException("No implementado aún")
+    if (n>=0){
+      var listaInterna = List()
+      for (elm <- lista){
+        var listaInterna: List[Int] = List()
+        for (i <- 1 to n){
+          listaInterna = listaInterna :+ elm
+        }
+        listaRepetida = listaRepetida :+ listaInterna
+      }
+      return listaRepetida
+    }
+    else {
+      throw new IllegalArgumentException( "n debe ser natural positivo")
+    }
   }
   /*
   * Punto 3: Filtrar listas
@@ -28,7 +40,44 @@ class EjercicioListas {
   def filtrarListas(criterioIn: String, n: Int, lista: List[Int]) : List[Int] = {
     var criterio : String = criterioIn.toLowerCase()
     var listaFiltrada : List[Int] = List()
-    //Complete el código
-    throw new UnsupportedOperationException("No implementado aún")
+    var funcion = (x: Int, y: Int) => x>y
+    if (criterio == "menor"){
+      funcion = (x: Int,y: Int )=> x<y
+    }
+    else {
+      if (criterio == "mayoroigual"){
+        funcion = (x: Int,y: Int) => x>=y
+      }
+      else {
+        if (criterio == "menoroigual") {
+          funcion = (x: Int, y: Int) => x <= y
+        }
+        else {
+          if (criterio == "igual") {
+            funcion = (x: Int, y: Int) => x == y
+          }
+          else {
+            if (criterio == "diferente"){
+              funcion = (x: Int, y: Int) => x != y
+            }
+            else {
+              if (criterio == "mayor") {
+                funcion = (x: Int, y: Int) => x > y
+              }
+              else{
+                throw new IllegalArgumentException("el criterio es invalido")
+              }
+            }
+          }
+        }
+      }
+    }
+
+    for (elm <- lista){
+      if (funcion(elm,n)){
+        listaFiltrada = listaFiltrada :+ elm
+      }
+    }
+    return listaFiltrada
   }
 }
--- app/src/main/scala/introduccion/ListaFactoriales.scala
+++ app/src/main/scala/introduccion/ListaFactoriales.scala
@@ -2,10 +2,16 @@ package introduccion
 
 
 class ListaFactoriales {
-    def objListaFactoriales(n : Int): List[BigInt] = {
-        var listaFactoriales : List[BigInt] = List()
+
+
+    def objListaFactoriales(n: Int): List[BigInt] = {
+        var listaFactoriales: List[BigInt] = List()
         val objFactorial: Factorial = new Factorial()
-        //Complete el código
-        throw new UnsupportedOperationException("No implementado aún")
+
+        for (i <- 0 to n) {
+            listaFactoriales = listaFactoriales :+ objFactorial.calculateFactorial(i)
+        }
+
+        return listaFactoriales
     }
 }
